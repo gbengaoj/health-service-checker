344a0a26b4187debc382cb95f933bf836bd8beb9
Totoola Kenny <40864358+Totoola-Kehinde@users.noreply.github.com>
2020-06-09 18:56:14 +0100

    added Timer

--- app.py
+++ app.py
@@ -4,35 +4,41 @@ from healthchecks.services.manage_pages import ManagePageService
 from healthchecks.services.email_notifications import EmailService
 from healthchecks.services.company_settings import CompanySettingsService
 from healthchecks.services.auth import AuthService
+import threading
 
-# Managing Users Microservices Health Check
-Userhandler = UserService()
-Userhandler.create_service_url()
-UserhandlerHealthCheck = Userhandler.process_services(Userhandler.services)
+def healthChecktimer():
+    threading.Timer(600.0, healthChecktimer).start()
+    # Managing Users Microservices Health Check
+    Userhandler = UserService()
+    Userhandler.create_service_url()
+    UserhandlerHealthCheck = Userhandler.process_services(Userhandler.services)
 
-# SMS Notifications Microservices Health Check
-SmsNotificationHandler = SmsService()
-SmsNotificationHandler.create_service_url()
-SmsNotificationHandlerHealthCheck = SmsNotificationHandler.process_services(SmsNotificationHandler.services)
+    # SMS Notifications Microservices Health Check
+    SmsNotificationHandler = SmsService()
+    SmsNotificationHandler.create_service_url()
+    SmsNotificationHandlerHealthCheck = SmsNotificationHandler.process_services(SmsNotificationHandler.services)
 
-# Managing Static and External Pages Microservices Health Check
-ManagePageHandler = ManagePageService()
-ManagePageHandler.create_service_url()
-ManagePageHandlerHealthCheck = ManagePageHandler.process_services(ManagePageHandler.services)
+    # Managing Static and External Pages Microservices Health Check
+    ManagePageHandler = ManagePageService()
+    ManagePageHandler.create_service_url()
+    ManagePageHandlerHealthCheck = ManagePageHandler.process_services(ManagePageHandler.services)
 
-# Email Notifications Microservices Health Check
-EmailHandler = EmailService()
-EmailHandler.create_service_url()
-EmailHandlerHealthCheck = EmailHandler.process_services(EmailHandler.services)
+    # Email Notifications Microservices Health Check
+    EmailHandler = EmailService()
+    EmailHandler.create_service_url()
+    EmailHandlerHealthCheck = EmailHandler.process_services(EmailHandler.services)
 
-# Managing Company Settings Microservices Health Check
-CompanyHandler = CompanySettingsService()
-CompanyHandler.create_service_url()
-CompanyHandlerHealthCheck = CompanyHandler.process_services(CompanyHandler.services)
+    # Managing Company Settings Microservices Health Check
+    CompanyHandler = CompanySettingsService()
+    CompanyHandler.create_service_url()
+    CompanyHandlerHealthCheck = CompanyHandler.process_services(CompanyHandler.services)
 
-# Authentication Microservices Health Check
-AuthHandler = AuthService()
-AuthHandler.create_service_url()
-AuthHandlerHealthCheck = AuthHandler.process_services(AuthHandler.services)
+    # Authentication Microservices Health Check
+    AuthHandler = AuthService()
+    AuthHandler.create_service_url()
+    AuthHandlerHealthCheck = AuthHandler.process_services(AuthHandler.services)
 
-print(UserhandlerHealthCheck, SmsNotificationHandlerHealthCheck, ManagePageHandlerHealthCheck, EmailHandlerHealthCheck, CompanyHandlerHealthCheck, AuthHandlerHealthCheck)
+    print(UserhandlerHealthCheck, SmsNotificationHandlerHealthCheck, ManagePageHandlerHealthCheck, EmailHandlerHealthCheck, CompanyHandlerHealthCheck, AuthHandlerHealthCheck)
+
+
+healthChecktimer()
